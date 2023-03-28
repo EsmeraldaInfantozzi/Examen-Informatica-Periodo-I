692131272bf0ebfec605d40a4c013647fa6d946f
EsmeraldaInfantozzi <124709439+EsmeraldaInfantozzi@users.noreply.github.com>
2023-03-28 02:30:23 -0300

    Update ejercicio 10.py

--- ejercicio 10.py
+++ ejercicio 10.py
@@ -1,30 +1,30 @@
 sueldo = float(input("Ingrese su sueldo mensual: "))
 
-if sueldo <= 472:
+if sue <= 472:
     impuesto = 0
-    cuota_fija = 0
-elif sueldo <= 895.24:
-    exceso = sueldo - 472
+    cuota_fij = 0
+elif sue <= 895.24:
+    exceso = sue - 472
     impuesto = exceso * 0.1
-    cuota_fija = 17.67
-elif sueldo <= 2038.10:
-    exceso = sueldo - 895.24
+    cuota_fij = 17.67
+elif sue <= 2038.10:
+    exceso = sue - 895.24
     impuesto = 42.53 + (exceso * 0.2)
-    cuota_fija = 60
+    cuota_fij = 60
 else:
-    exceso = sueldo - 2038.10
+    exceso = sue - 2038.10
     impuesto = 352.28 + (exceso * 0.3)
-    cuota_fija = 288.57
+    cuota_fij = 288.57
 
-iss = sueldo * 0.03
-afp = sueldo * 0.0725
-total_descuentos = iss + afp + impuesto + cuota_fija
-sueldo_neto = sueldo - total_descuentos
+iss = sue * 0.03
+afp = sue * 0.0725
+total_descuentos = iss + afp + impuesto + cuota_fij
+suel_neto = sue - total_descuentos
 
-print("Sueldo bruto: ${:.2f}".format(sueldo))
+print("Sueldo bruto: ${:.2f}".format(sue))
 print("Impuesto sobre la renta: ${:.2f}".format(impuesto))
 print("ISS: ${:.2f}".format(iss))
 print("AFP: ${:.2f}".format(afp))
-print("Cuota fija: ${:.2f}".format(cuota_fija))
+print("Cuota fija: ${:.2f}".format(cuota_fij))
 print("Total descuentos: ${:.2f}".format(total_descuentos))
-print("Sueldo neto: ${:.2f}".format(sueldo_neto))
+print("Sueldo neto: ${:.2f}".format(suel_neto))
